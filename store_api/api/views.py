7958a1f2c18b89bbf13297c09163e37fce5b5915
Jon Ventura <jonathan_vmaced@hotmail.com>
2025-03-25 14:47:00 -0300

    [FEAT]: added product info and better query to orders

--- store_api/api/views.py
+++ store_api/api/views.py
@@ -1,9 +1,11 @@
+from django.db.models import Max
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from api.models import Order, OrderItem, Product
-from api.serializers import OrderSerializer, ProductSerializer
+from api.serializers import (OrderSerializer, ProductInfoSerializer,
+                             ProductSerializer)
 
 
 @api_view(['GET'])
@@ -20,6 +22,16 @@ def product_detail(request, pk):
 
 @api_view(['GET'])
 def order_list(request):
-    orders = Order.objects.all()
+    orders = Order.objects.prefetch_related('items', 'items__product').all()
     serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        'products': products,
+        'count': products.count(),
+        'max_price': products.aggregate(max_price=Max('price'))['max_price'],
+    })
     return Response(serializer.data)
