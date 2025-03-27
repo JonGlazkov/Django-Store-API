43feb620132455d1a96f717cb5126c3dac1cb6a8
Jon Ventura <jonathan_vmaced@hotmail.com>
2025-03-26 23:50:20 -0300

    [FEAT]: Added authenticated endpoint and test for it

--- store_api/api/views.py
+++ store_api/api/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Max
-from django.shortcuts import get_object_or_404
+from rest_framework import generics
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from api.models import Order, OrderItem, Product
@@ -8,23 +9,29 @@ from api.serializers import (OrderSerializer, ProductInfoSerializer,
                              ProductSerializer)
 
 
-@api_view(['GET'])
-def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.filter(stock__gt=0)
+    serializer_class = ProductSerializer
 
-@api_view(['GET'])
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = 'product_id'
 
-@api_view(['GET'])
-def order_list(request):
-    orders = Order.objects.prefetch_related('items', 'items__product').all()
-    serializer = OrderSerializer(orders, many=True)
-    return Response(serializer.data)
+class OrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items', 'items__product').all()
+    serializer_class = OrderSerializer
+
+class UserOrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items', 'items__product').all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+ 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(user=self.request.user)
+
+
 
 @api_view(['GET'])
 def product_info(request):
